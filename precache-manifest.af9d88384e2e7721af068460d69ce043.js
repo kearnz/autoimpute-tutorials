d197ec9ddffb0d0102fabd29ed9d9a208854f6a1
kearnz <josephkearney14@gmail.com>
2019-06-05 20:48:01 -0400

    Updates

---
+++ precache-manifest.af9d88384e2e7721af068460d69ce043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd95a8417ef8e279cafc00403d68200b",
+    "url": "/autoimpute-tutorials/index.html"
+  },
+  {
+    "revision": "d579e81336ba483d2d2d",
+    "url": "/autoimpute-tutorials/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "10c105c13d0d09e8c0a5",
+    "url": "/autoimpute-tutorials/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d579e81336ba483d2d2d",
+    "url": "/autoimpute-tutorials/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "10c105c13d0d09e8c0a5",
+    "url": "/autoimpute-tutorials/static/js/main.d598a5bd.chunk.js"
+  },
+  {
+    "revision": "b904018510989bdcc0f8",
+    "url": "/autoimpute-tutorials/static/js/runtime~main.dfa0726d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/autoimpute-tutorials/precache-manifest.10f3d16f0e99c5068ffc5e4b0dad17f1.js"
+  "/autoimpute-tutorials/precache-manifest.af9d88384e2e7721af068460d69ce043.js"
 );
 
 self.addEventListener('message', (event) => {
